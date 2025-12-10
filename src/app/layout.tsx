a6082fa040e865022953674b4d3d89e82d3d3caf
Abdullah Al Masud <abdullahalmasud772@gmail.com>
2025-12-10 23:59:34 +0600

    add folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+'use client'
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
+import './page.module.css'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
