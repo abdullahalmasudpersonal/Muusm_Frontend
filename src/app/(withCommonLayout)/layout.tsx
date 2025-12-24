448c7fd60ba962fea288284e0868b74f95eb60cb
Abdullah Al Masud <abdullahalmasud772@gmail.com>
2025-12-24 23:53:57 +0600

    add navber & footer

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,10 +1,14 @@
+import Footer from "@/components/navigation/Footer";
+import Navber from "@/components/navigation/Navber";
 import React from "react";
 
 
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
+            <Navber />
             {children}
+            <Footer />
         </>
     );
 };
