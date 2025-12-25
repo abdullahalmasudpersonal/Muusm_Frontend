5f7b98873ad19a19d59a033d2956ed3fe0b1b228
Abdullah Al Masud <abdullahalmasud772@gmail.com>
2025-12-25 23:55:48 +0600

    another update

--- src/components/navigation/Navber.tsx
+++ src/components/navigation/Navber.tsx
@@ -1,11 +1,23 @@
-
+'use client';
+import React from 'react';
+import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Navber = () => {
-    return (
-        <div>
-            Navber
-        </div>
-    );
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  return (
+    <AppBar position="fixed" sx={{
+      boxShadow: 0,
+      // bgcolor: "transparent",
+      // backgroundImage: "none",
+      mt: 2,
+    }}>
+      <Container maxWidth='lg'>
+        mmm
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Navber;
